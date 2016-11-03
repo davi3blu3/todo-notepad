4eeec5679028d251c24b5663bacbc566aa85b127
Dave Harned <dharned@gmail.com>
2016-11-02 23:51:29 -0500

    successful AJAX call to local JSON file

--- react/Paper.js
+++ react/Paper.js
@@ -1,14 +1,33 @@
 const React = require('react')
+const Axios = require('axios')
 
 const Paper = React.createClass({
+    getInitialState: function() {
+        return {
+            toDoItems: []
+        }
+    },
+    componentDidMount: function() {
+        var th = this
+        this.serverRequest =
+        Axios.get('../react/data.json')
+            .then(function(result) {
+                th.setState({
+                    toDoItems: result.data.items
+                })
+            })
+    },
+    componentWillUnmount: function() {
+        this.serverRequest.abort()
+    },
     render() {
-        var toDoItems = ["Wake Up", "Shower", "Let Dexter Out", "Make Breakfast", "Go To Work", "Go To Gym", "Make Spaghetti", "Write Code!", "Brush Teeth", "Go To Sleep"]
+        // var toDoItems = ["Monday", "Tuesday", "Wednesday", "Thursday"]
         return (
             <div className="container paper">
                 <div className="lines"></div>
                 <h2 className="title">To-Do List</h2>
                 <ul className="list">
-                    {toDoItems.map(function(item, index){
+                    {this.state.toDoItems.map(function(item, index){
                         return (
                             <div key={index}>
                                 <img src="img/check.png" alt="check mark" className="done" /><li className="">{item}<img src="img/delete.png" alt="delete x mark" className="delete" /><img src="img/pencil.png" alt="pencil icon" className="edit" /></li>
