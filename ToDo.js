0f41367217a266984b8fd936440eaf842e4b011a
Dave Harned <dharned@gmail.com>
2016-11-02 22:24:11 -0500

    setup React environment

---
+++ ToDo.js
@@ -0,0 +1,10 @@
+const React = require('react')
+const ReactDOM = require('react-dom')
+
+const ToDoApp = (
+    <div className="container">
+        <h1>HI MOM!</h1>
+    </div>
+)
+
+ReactDOM.render(ToDoApp, document.getElementById('app'))
