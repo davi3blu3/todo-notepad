30180ec0ac88528338d20740af123beaee6c348e
Dave Harned <dharned@gmail.com>
2016-11-03 21:18:49 -0500

    restyle css hover styles

--- public/scripts/bundle.js
+++ public/scripts/bundle.js
@@ -22972,14 +22972,17 @@
 
 	var ListItem = React.createClass({
 	    displayName: "ListItem",
+	    getInitialState: function getInitialState() {
+	        return { "showIcons": "hidden" };
+	    },
 	    render: function render() {
 	        return React.createElement(
 	            "div",
 	            null,
 	            React.createElement(
 	                "li",
-	                { className: "" },
-	                React.createElement("img", { src: "img/check.png", alt: "check mark", className: "done" }),
+	                null,
+	                React.createElement("img", { src: "img/check.png", alt: "check mark", className: "check" }),
 	                this.props.item,
 	                React.createElement("img", { src: "img/delete.png", alt: "delete x mark", className: "delete" })
 	            )
--- react/ListItem.js
+++ react/ListItem.js
@@ -2,11 +2,14 @@ const React = require('react')
 
 
 const ListItem = React.createClass({
+    getInitialState(){
+        return {"showIcons": "hidden"}
+    },
     render() {
         return (
             <div>
-                <li className="">
-                    <img src="img/check.png" alt="check mark" className="done" />
+                <li>
+                    <img src="img/check.png" alt="check mark" className="check" />
                     {this.props.item}
                     <img src="img/delete.png" alt="delete x mark" className="delete" />
                 </li>
